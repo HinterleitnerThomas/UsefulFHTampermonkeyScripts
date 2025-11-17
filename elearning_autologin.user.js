43624f400f6577fd7d8cf503f1063840ce495ac7
Thomas Hinterleitner <s2510458002@students.fh-hagenberg.at>
2025-11-17 20:47:59 +0100

    Updated match pattern, otherwise ?loginredirect=1 prevents script from working

--- elearning_autologin.user.js
+++ elearning_autologin.user.js
@@ -4,7 +4,7 @@
 // @version      2025-11-17_2
 // @description  Clicks the login button on eLearning
 // @author       You
-// @match        https://elearning.fh-ooe.at/login/index.php
+// @match        https://elearning.fh-ooe.at/login/index.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=fh-ooe.at
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/HinterleitnerThomas/UsefulFHTampermonkeyScripts/refs/heads/main/elearning_autologin.user.js
