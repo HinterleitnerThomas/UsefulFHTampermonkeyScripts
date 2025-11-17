bddaa6126de95e1a294e05f5c2b33ce2491fa9ab
Thomas Hinterleitner <s2510458002@students.fh-hagenberg.at>
2025-11-17 17:36:00 +0100

    Added updateURL

--- elearning_autologin.user.js
+++ elearning_autologin.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         eLearning Autologin
 // @namespace    http://tampermonkey.net/
-// @version      2025-11-17
+// @version      2025-11-17_2
 // @description  Clicks the login button on eLearning
 // @author       You
 // @match        https://elearning.fh-ooe.at/login/index.php
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=fh-ooe.at
 // @grant        none
+// @downloadURL  https://raw.githubusercontent.com/HinterleitnerThomas/UsefulFHTampermonkeyScripts/refs/heads/main/elearning_autologin.user.js
+// @updateURL    https://raw.githubusercontent.com/HinterleitnerThomas/UsefulFHTampermonkeyScripts/refs/heads/main/elearning_autologin.user.js
 // ==/UserScript==
 
 (function() {
